63ae7f67c8df116f9aa95a48b0dc2db7494df63c
Chao Wang <chaowang0524@outlook.com>
2022-12-25 20:04:51 +0800

    25 Dec

--- Practises/40. Reverse_a_list.py
+++ Practises/40. Reverse_a_list.py
@@ -1,11 +1,11 @@
-lst = [1, 4, 6, 9, 13, 19, 20, 88, 96, 45]
+
 # if the length of the list is even:
 # swap the first and last elements, the second and the second last
 # if the length of the list is odd:
 # 1. swap the first and last elements, the second and the second last
 # 2. keep the middle number
 
-
+lst = [8,2,3,4,5,6,7,8,9,10,11,12,13]
 def freverse(a: list) -> list:
     """Output a reversed version of the given list
 
@@ -18,13 +18,14 @@ def freverse(a: list) -> list:
 
     # if the length of the list is even:
     if len(a) % 2 == 0:
-        for i in range(1, int((len(a)/2)+1)):
+        for i in range(1, ((len(a)/2)+1)): # start swaping
             # swap the first and last elements, the second and the second last...
             # set the pointers
             a[i-1], a[-i] = a[-i], a[i-1]
-    else:
-        for i in range(1, (len(a)//2)+1):
-            if a[i-1] != a[(len(a)//2)+1]:
+    else: # if it is odd:
+        # start swaping from first number to the middle number(include)
+        for i in range(1, (len(a)//2)+1): 
+            if a[i-1] != a[(len(a)//2)+1]: #
                 a[i-1], a[-i] = a[-i], a[i-1]
     return(a)
 
--- Practises/practise.py
+++ Practises/practise.py
@@ -1,3 +1,2 @@
-import random
-
-print(random.randint(1, 100))
+for i in range(1,5):
+    print(i)
